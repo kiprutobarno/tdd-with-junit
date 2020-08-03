3617037661b72c5c1f368101d417c691d13364c7
ywalakamar <barxwells@gmail.com>
2020-08-03 15:02:29 +0300

    chore(test): important annotations in java TDD - reconfigure .travis.yml

--- src/test/java/com/example/junit/MathTest.java
+++ src/test/java/com/example/junit/MathTest.java
@@ -2,6 +2,8 @@ package com.example.junit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -9,6 +11,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 public class MathTest {
     Math math = new Math();
+
+    @BeforeAll
+    public void before(){
+        System.out.println("Before");
+    }
+
+    @AfterAll
+    public void after(){
+        System.out.println("After");
+    }
 
     @Test
     public void SumTwoNumbers() {
